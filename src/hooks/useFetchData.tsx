2c5d84f9af519bdfbf545951cba747e8211cdbb1
jel123 <quinamotjeland@gmail.com>
2023-07-05 19:27:32 +0800

    Added environmenet variables

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -1,15 +1,17 @@
 import axios from "axios";
 
 function useFetchData(url: string = "") {
+  const serverUrl = process.env.REACT_APP_SERVER_URL;
+
   const fetchData = () => {
     const request = axios.get(
-      `http://localhost:3001/api/data/${encodeURIComponent(url)}`
+      `${serverUrl}api/data/${encodeURIComponent(url)}`
     );
     return request.then((res) => res);
   };
   const downloadImage = async (urlImage: string) => {
     const request = await axios.get(
-      `http://localhost:3001/api/downloadImage/${encodeURIComponent(urlImage)}`
+      `${serverUrl}api/downloadImage/${encodeURIComponent(urlImage)}`
     );
     return request;
   };
